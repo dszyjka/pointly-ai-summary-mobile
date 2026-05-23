03e1f5be569cde119852152faff32454a5ad8d38
Dawid Szyjka <dawid.szyjkaa1910@interia.pl>
2026-05-24 01:18:39 +0200

    feat: add data class Summary

---
+++ app/src/main/java/com/example/pointlyaisummary/jsonStructures.kt
@@ -0,0 +1,15 @@
+package com.example.pointlyaisummary
+
+import android.icu.text.DateFormat
+import com.google.gson.annotations.SerializedName
+
+
+data class Summary (
+    val id: Int,
+    val userId: String,
+    @SerializedName("filename")
+    val fileName: String,
+    val summary: String,
+    @SerializedName("created_at")
+    val createdAt: DateFormat
+)
